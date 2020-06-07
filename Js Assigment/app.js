7ab1ea4f180c53c193cf7f917fdf9a07656a0bbb
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2020-06-07 04:36:36 -0700

    Working on the js assignment

---
+++ Js Assigment/app.js
@@ -0,0 +1,136 @@
+// Chapter 1
+
+//1 
+
+// alert("Hello How are you?");
+
+// //2
+
+// alert("Error! Please enter a valid password!");
+
+// //3
+
+// alert("Welcome to Js Land \n Happy Coding!");
+
+// 4
+
+
+// alert("Welcome to Js Land");
+
+// alert("Happy Coding!")
+
+// Chapter 2
+
+// var username;
+// var myName = "Abdul Rafay Khan";
+// var message;
+// message = "Hello World";
+// alert(message);
+
+// var age = 24;
+// var certificate = "BS(CS)";
+
+// alert(myName);
+// alert(age);
+// alert(certificate);
+
+// var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(pizza);
+
+// var email = "rafaykhan.work@gmail.com";
+// alert("My email address is" + email);
+
+// var book = "A smarter way to learn Javascript";
+// alert(book);
+
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// var something = "-----------------$@#$R----------------";
+// alert(something);
+
+
+// Chapter 3
+
+// var age = 24;
+// alert("I am " + age + " years old");
+
+// var visitCount = 23;
+// alert("You have visited the site " + visitCount + " times");
+
+// var birthYear;
+// birthYear = 1995;
+// document.write("My birth year is " + birthYear);
+
+// var visitorName = "Shehroz";
+// var productTitle = "Milk";
+// var productQuantity = 10;
+
+// document.write(visitorName + " ordered " + productQuantity + " " + productTitle + " on Kiryana Store");
+
+
+
+//Chapter 4
+
+// var a,b,c;
+
+// // var 1a,#a,$ac,.x,;
+// var a,a_,_a,cx,s2;
+
+// document.write("Rules for naming JS Varibales \n \n");
+
+// document.write("Variable name can only contain numbers, $ and _ \n");
+
+// document.write("Variable name must only begin with letter $ or _ \n");
+
+
+//Chapter 5 
+
+var numberOne = 10;
+var numberTwo = 8;
+
+document.write("Sum of " + numberOne + " and " + numberTwo + " is " + numberOne+numberTwo + "\n");
+document.write("Subtract of " + numberOne + " and " + numberTwo + " is " + numberOne-numberTwo + "\n");
+document.write("Multiplication of " + numberOne + " and " + numberTwo + " is " + numberOne*numberTwo + "\n");
+document.write("Division of " + numberOne + " and " + numberTwo + " is " + numberOne/numberTwo + "\n");
+
+
+var myVariable;
+document.write("Value after variable declaration is: " + myVariable);
+myVariable = 5;
+document.write(" \n Value after variable intialzation is: " + myVariable);
+myVariable = myVariable + 1;
+document.write(" \nValue after variable increment is: " + myVariable);
+myVariable = myVariable +  7;
+document.write("\n Value after addition is: " + myVariable);
+myVariable = myVariable - 1;
+document.write("\n Value after variable decrement is: " + myVariable);
+document.write("\n After dividing the remainder is: " + myVariable%3);
+
+
+var ticketPrice = 600;
+document.write("\n\n \n \n Total cost to buy 5 tickets to a movie is " + 5*ticketPrice+ "PKR");
+
+var tableNumber = 4;
+document.write("\n \n \nTable of 4\n");
+document.write("4 X 1 = " + " " +  tableNumber*1 + "\n");
+document.write("4 X 2 = " + " " +  tableNumber*2 + "\n");
+document.write("4 X 3 = " + " " +  tableNumber*3 + "\n");
+document.write("4 X 4 = " + " " +  tableNumber*4 + "\n");
+document.write("4 X 5 = " + " " +  tableNumber*5 + "\n");
+document.write("4 X 6 = " + " " +  tableNumber*6 + "\n");
+document.write("4 X 7 = " + " " +  tableNumber*7 + "\n");
+document.write("4 X 8 = " + " " +  tableNumber*8 + "\n");
+document.write("4 X 9 = " + " " +  tableNumber*9 + "\n");
+document.write("4 X 10 = " + " " +  tableNumber*10 + "\n");
+
+
+
+
+
+var celciusTemperature = 40;
+var convertCelciusToFarenheit = (celciusTemperature * 9/5) + 32;
+var fahrenheitTemperature = 100;
+var convertFahrenheritToCelcius = (fahrenheitTemperature - 32) * (5/9);
+
+document.write("\n " + celciusTemperature + "C is " + convertCelciusToFarenheit + "\n");
+document.write("\n " + fahrenheitTemperature + "F is " + convertFahrenheritToCelcius + "\n");
