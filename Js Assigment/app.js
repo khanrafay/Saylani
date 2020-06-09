7d9c2e1ffb533f60fa6785baa03e5fd2a47c0217
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2020-06-09 12:49:01 -0700

    Done with chap 5

--- Js Assigment/app.js
+++ Js Assigment/app.js
@@ -1,31 +1,47 @@
 // Chapter 1
 
-//1 
+//Task 1 
 
 // alert("Hello How are you?");
 
-// //2
+////Task 2 
 
 // alert("Error! Please enter a valid password!");
 
-// //3
+//Task 3
 
 // alert("Welcome to Js Land \n Happy Coding!");
 
-// 4
+//Task 4
 
 
 // alert("Welcome to Js Land");
 
 // alert("Happy Coding!")
 
+
+
+
+
 // Chapter 2
 
+//Task 1 
+
 // var username;
+
+//Task 2
+
 // var myName = "Abdul Rafay Khan";
+
+//Task 3
+
+
 // var message;
 // message = "Hello World";
 // alert(message);
+
+//Task 4
+
 
 // var age = 24;
 // var certificate = "BS(CS)";
@@ -34,16 +50,32 @@
 // alert(age);
 // alert(certificate);
 
+
+//Task 5
+
+
 // var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P";
 // alert(pizza);
+
+//Task 6
 
 // var email = "rafaykhan.work@gmail.com";
 // alert("My email address is" + email);
 
+
+//Task 7
+
 // var book = "A smarter way to learn Javascript";
 // alert(book);
 
+
+//Task 8
+
+
 // document.write("Yah! I can write HTML content through JavaScript");
+
+
+//Task 9
 
 // var something = "-----------------$@#$R----------------";
 // alert(something);
@@ -51,15 +83,24 @@
 
 // Chapter 3
 
+
+//Task 1
+
 // var age = 24;
 // alert("I am " + age + " years old");
+
+//Task 2
 
 // var visitCount = 23;
 // alert("You have visited the site " + visitCount + " times");
 
+//Task 3
+
 // var birthYear;
 // birthYear = 1995;
 // document.write("My birth year is " + birthYear);
+
+//Task 4
 
 // var visitorName = "Shehroz";
 // var productTitle = "Milk";
@@ -71,200 +112,253 @@
 
 //Chapter 4
 
+//Task 1
+
 // var a,b,c;
 
+//Task 2
 // // var 1a,#a,$ac,.x,;
 // var a,a_,_a,cx,s2;
 
+//Task 3
 // document.write("Rules for naming JS Varibales \n \n");
 
 // document.write("Variable name can only contain numbers, $ and _ \n");
 
 // document.write("Variable name must only begin with letter $ or _ \n");
 
-document.write("\n");
-document.write("\n");
-document.write("\n");
 
-document.write("Chaper 5 \n") 
+//Chapter 5
 
-document.write("\n");
-document.write("\n");
-document.write("\n");
-
-var numberOne = 10;
-var numberTwo = 8;
-
-document.write("\n Sum of " + numberOne + " and " + numberTwo + " is " + numberOne+numberTwo + "\n");
-document.write("Subtract of " + numberOne + " and " + numberTwo + " is " + numberOne-numberTwo + "\n");
-document.write("Multiplication of " + numberOne + " and " + numberTwo + " is " + numberOne*numberTwo + "\n");
-document.write("Division of " + numberOne + " and " + numberTwo + " is " + numberOne/numberTwo + "\n");
+//Task 1
 
 
-var myVariable;
-document.write("Value after variable declaration is: " + myVariable);
-myVariable = 5;
-document.write(" \n Value after variable intialzation is: " + myVariable);
-myVariable = myVariable + 1;
-document.write(" \nValue after variable increment is: " + myVariable);
-myVariable = myVariable +  7;
-document.write("\n Value after addition is: " + myVariable);
-myVariable = myVariable - 1;
-document.write("\n Value after variable decrement is: " + myVariable);
-document.write("\n After dividing the remainder is: " + myVariable%3);
+// var numberOne = 10;
+// var numberTwo = 8;
 
-
-var ticketPrice = 600;
-document.write("\n\n \n \n Total cost to buy 5 tickets to a movie is " + 5*ticketPrice+ "PKR");
-
-var tableNumber = 4;
-document.write("\n \n \nTable of 4\n");
-document.write("4 X 1 = " + " " +  tableNumber*1 + "\n");
-document.write("4 X 2 = " + " " +  tableNumber*2 + "\n");
-document.write("4 X 3 = " + " " +  tableNumber*3 + "\n");
-document.write("4 X 4 = " + " " +  tableNumber*4 + "\n");
-document.write("4 X 5 = " + " " +  tableNumber*5 + "\n");
-document.write("4 X 6 = " + " " +  tableNumber*6 + "\n");
-document.write("4 X 7 = " + " " +  tableNumber*7 + "\n");
-document.write("4 X 8 = " + " " +  tableNumber*8 + "\n");
-document.write("4 X 9 = " + " " +  tableNumber*9 + "\n");
-document.write("4 X 10 = " + " " +  tableNumber*10 + "\n");
+// document.write("\n Sum of " + numberOne + " and " + numberTwo + " is " + numberOne+numberTwo + "\n");
 
 
 
+// document.write('<br>');
+
+// //Task 2
+
+// document.write("Subtract of " + numberOne + " and " + numberTwo + " is " + numberOne-numberTwo + "\n");
+// document.write("Multiplication of " + numberOne + " and " + numberTwo + " is " + numberOne*numberTwo + "\n");
+// document.write("Division of " + numberOne + " and " + numberTwo + " is " + numberOne/numberTwo + "\n");
+
+//  document.write('<br>');
+
+ 
+// //Task 3
+
+// var myVariable;
+// document.write("Value after variable declaration is: " + myVariable);
+// myVariable = 5;
+// document.write(" \n Value after variable intialzation is: " + myVariable);
+// myVariable = myVariable + 1;
+// document.write(" \nValue after variable increment is: " + myVariable);
+// myVariable = myVariable +  7;
+// document.write("\n Value after addition is: " + myVariable);
+// myVariable = myVariable - 1;
+// document.write("\n Value after variable decrement is: " + myVariable);
+// document.write("\n After dividing the remainder is: " + myVariable%3);
 
 
-var celciusTemperature = 40;
-var convertCelciusToFarenheit = (celciusTemperature * 9/5) + 32;
-var fahrenheitTemperature = 100;
-var convertFahrenheritToCelcius = (fahrenheitTemperature - 32) * (5/9);
-
-document.write("\n " + celciusTemperature + "C is " + convertCelciusToFarenheit + "\n");
-document.write("\n " + fahrenheitTemperature + "F is " + convertFahrenheritToCelcius + "\n");
+// document.write('<br>');
 
 
-var item1 = 1000;
-var item2 = 600;
-var item1Quantity = 3;
-var item2Quantity = 6;
-var shippingCharges = 90;
+// //Task 4
 
-document.write("\n");
-document.write("Price of item 1 is " + item1);
-document.write("\n");
-document.write("Quantity of item 1 is " + item1Quantity);
-document.write("\n");
-document.write("Price of item 2 is " + item2);
-document.write("\n");
-document.write("Quantity of item 2 is " + item2Quantity);
-document.write("\n");
-document.write("Shipping Charges " + shippingCharges);
-document.write("\n");
-document.write("\n");
-document.write("Total cost of your order is 2750");
-document.write("\n");
-document.write("\n");
+// var ticketPrice = 600;
+// document.write("\n\n \n \n Total cost to buy 5 tickets to a movie is " + 5*ticketPrice+ "PKR");
 
 
-
-
-
-var totalMarks = 980;
-var marksObtained = 800;
-
-document.write("\n");
-document.write("Mark Sheet");
-document.write("Total Marks: " + totalMarks);
-document.write("\n");
-document.write("Marks Obtained " + marksObtained);
-document.write("\n");
-document.write("Percentage: " + (totalMarks/marksObtained) * 100);
-document.write("\n");
-document.write("\n");
+// document.write('<br>');
 
 
 
-var dollarRate = 104.80;
-var riyalRate = 28;
-var dollarQuantity = 10;
-var riyalQuantity = 25;
-
-var dollarIntoPkr = dollarRate * dollarQuantity;
-var riyalIntoPkr = riyalRate * riyalQuantity;
-
-document.write("\n");
-document.write("Currency in PKR");
-document.write("\n");
-document.write("\n");
-
-document.write("Total currency in PKR: " + dollarIntoPkr + riyalIntoPkr);
-
-document.write("\n");
-document.write("\n");
-document.write("\n");
+// //Task 5
 
 
-var someNumber = 2;
-var someNumberResult = (someNumber + 5 * 10) / 2;
+// var tableNumber = 4;
+// document.write("\n \n \nTable of 4\n");
+// document.write("4 X 1 = " + " " +  tableNumber*1 + "\n");
+// document.write("4 X 2 = " + " " +  tableNumber*2 + "\n");
+// document.write("4 X 3 = " + " " +  tableNumber*3 + "\n");
+// document.write("4 X 4 = " + " " +  tableNumber*4 + "\n");
+// document.write("4 X 5 = " + " " +  tableNumber*5 + "\n");
+// document.write("4 X 6 = " + " " +  tableNumber*6 + "\n");
+// document.write("4 X 7 = " + " " +  tableNumber*7 + "\n");
+// document.write("4 X 8 = " + " " +  tableNumber*8 + "\n");
+// document.write("4 X 9 = " + " " +  tableNumber*9 + "\n");
+// document.write("4 X 10 = " + " " +  tableNumber*10 + "\n");
 
 
 
-var currentYear = 2020;
-var birthYear = 1995;
-var myAge = 2020-1995;
-
-document.write("\n");
-document.write("\n");
-document.write("Age Calculator");
-
-document.write("\n");
-document.write("Current Year: " + currentYear );
-document.write("\n");
-document.write("Birth Year: " + birthYear);
-document.write("\n");
-document.write("Your age: " + myAge);
-
-document.write("\n");
-document.write("\n");
-document.write("\n");
+// document.write('<br>');
 
 
-var radius = 20;
-document.write("\n");
-document.write("Radius of a Circle is " + radius);
-document.write("\n");
-document.write("The circumference is: "  + 2 * 3.142 * radius);
-document.write("\n");
-document.write("The area is: " + 3.142 * (radius*radius));
+// //Task 6
 
-document.write("\n");
-document.write("\n");
-document.write("\n");
+// var celciusTemperature = 40;
+// var convertCelciusToFarenheit = (celciusTemperature * 9/5) + 32;
+// var fahrenheitTemperature = 100;
+// var convertFahrenheritToCelcius = (fahrenheitTemperature - 32) * (5/9);
 
 
-var favoriteSnack = "kurkure";
-var myAge = 24;
-var maximumAge = 70;
-var amountPerday = 1;
+// document.write("\n " + celciusTemperature + "C is " + convertCelciusToFarenheit + "\n");
+// document.write("\n " + fahrenheitTemperature + "F is " + convertFahrenheritToCelcius + "\n");
 
-document.write("\n");
-document.write("\n");
-document.write("The Lifetime Supply Calculator");
-document.write("\n");
-document.write("\n");
-document.write("\n");
-
-document.write("Favorite Snack: " + favoriteSnack);
-document.write("\n");
-document.write("Current Age: " + myAge);
-document.write("\n");
-document.write("Estimated Maximum: " + maximumAge);
-document.write("\n");
-document.write("Amout of snacks per day: " + amountPerday);
-document.write("\n");
-document.writeln("\n You will need " + (maximumAge - myAge) * amountPerday + " " + favoriteSnack + " to last you until the ripe old age of " + maximumAge );
+// document.write('<br>');
 
 
+// //Task 7
+
+// var item1 = 1000;
+// var item2 = 600;
+// var item1Quantity = 3;
+// var item2Quantity = 6;
+// var shippingCharges = 90;
+
+// document.write("\n");
+// document.write("Price of item 1 is " + item1);
+// document.write("\n");
+// document.write("Quantity of item 1 is " + item1Quantity);
+// document.write("\n");
+// document.write("Price of item 2 is " + item2);
+// document.write("\n");
+// document.write("Quantity of item 2 is " + item2Quantity);
+// document.write("\n");
+// document.write("Shipping Charges " + shippingCharges);
+// document.write("\n");
+// document.write("\n");
+// document.write("Total cost of your order is 2750");
+// document.write("\n");
+// document.write("\n");
+
+
+
+// document.write('<br>');
+
+
+
+// //Task 8
+
+
+// var totalMarks = 980;
+// var marksObtained = 800;
+
+
+// document.write("Mark Sheet");
+// document.write("Total Marks: " + totalMarks);
+// document.write("\n");
+// document.write("Marks Obtained " + marksObtained);
+// document.write("\n");
+// document.write("Percentage: " + (totalMarks/marksObtained) * 100);
+
+
+// document.write('<br>');
+
+
+
+// //Task 9
+
+
+// var dollarRate = 104.80;
+// var riyalRate = 28;
+// var dollarQuantity = 10;
+// var riyalQuantity = 25;
+
+// var dollarIntoPkr = dollarRate * dollarQuantity;
+// var riyalIntoPkr = riyalRate * riyalQuantity;
+
+
+// document.write("Currency in PKR");
+
+
+// document.write("Total currency in PKR: " + dollarIntoPkr + riyalIntoPkr);
+
+
+
+// //Task 10
+
+
+// var someNumber = 2;
+// var someNumberResult = (someNumber + 5 * 10) / 2;
+
+
+
+// var currentYear = 2020;
+// var birthYear = 1995;
+// var myAge = 2020-1995;
+
+// document.write("\n");
+// document.write("\n");
+
+
+// //Task 11
+
+// document.write("Age Calculator");
+
+// document.write("\n");
+// document.write("Current Year: " + currentYear );
+// document.write("\n");
+// document.write("Birth Year: " + birthYear);
+// document.write("\n");
+// document.write("Your age: " + myAge);
+
+// document.write("\n");
+// document.write("\n");
+// document.write("\n");
+
+
+// //Task 12
+
+// var radius = 20;
+// document.write("\n");
+// document.write("Radius of a Circle is " + radius);
+// document.write("\n");
+// document.write("The circumference is: "  + 2 * 3.142 * radius);
+// document.write("\n");
+// document.write("The area is: " + 3.142 * (radius*radius));
+
+// document.write("\n");
+// document.write("\n");
+// document.write("\n");
+
+
+// //Task 12
+
+// var favoriteSnack = "kurkure";
+// var myAge = 24;
+// // var maximumAge = 70;
+// var amountPerday = 1;
+
+// document.write("\n");
+// document.write("\n");
+// document.write("The Lifetime Supply Calculator");
+// document.write("\n");
+// document.write("\n");
+// document.write("\n");
+
+// document.write("Favorite Snack: " + favoriteSnack);
+// document.write("\n");
+// document.write("Current Age: " + myAge);
+// document.write("\n");
+// document.write("Estimated Maximum: " + maximumAge);
+// document.write("\n");
+// document.write("Amout of snacks per day: " + amountPerday);
+// document.write("\n");
+// document.writeln("\n You will need " + (maximumAge - myAge) * amountPerday + " " + favoriteSnack + " to last you until the ripe old age of " + maximumAge );
+
+
+
+//Chapter 6-9
+
+
+//Task 1
 
 
 
