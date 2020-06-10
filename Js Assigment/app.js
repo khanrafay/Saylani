13208c59ded09b4d165720d1cb021622b1b94e7b
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2020-06-10 13:11:47 -0700

    Completed chap 11-13

--- Js Assigment/app.js
+++ Js Assigment/app.js
@@ -678,13 +678,75 @@
 
 // Task 2
 
-var firstNumber = +prompt("Enter the first number");
-var secondNumber = +prompt("Enter the second number");
+// var firstNumber = +prompt("Enter the first number");
+// var secondNumber = +prompt("Enter the second number");
 
-if(firstNumber === secondNumber){
-    document.write("Both are equal");
-}else if(firstNumber > secondNumber){
-    document.write("Larger number: "+ firstNumber)
-}else{
-    document.write("Larger Number: "+ secondNumber):
-}
+// if(firstNumber === secondNumber){
+//     document.write("Both are equal");
+// }else if(firstNumber > secondNumber){
+//     document.write("Larger number: "+ firstNumber)
+// }else{
+//     document.write("Larger Number: "+ secondNumber);
+// }
+
+
+//Task 3
+
+// var userInput = +prompt("Enter the number");
+
+// if(userInput === 0){
+//     document.write("It zero");
+// }else if(userInput < 0){
+//     document.write("It's negative");
+// }else if(userInput > 0){
+//     document.write("It's positive");
+// }
+
+//Task 4
+
+// var userInput = prompt("Enter any one character to know if it's vowel");
+
+// if(userInput === 'a' || userInput === 'e' || userInput === 'i' || userInput === 'o' || userInput === 'u' )
+// {
+//     document.write("It's a vowel");
+// }
+
+//Task 5
+
+// var correctPassword = "abc";
+// var inputPassword = prompt("Enter your password");
+
+// if(inputPassword === correctPassword){
+//     document.write("Correct! The password you have entered matches the original password");
+// }else{
+//     document.write("Incorrect Password");
+// }
+
+
+// Task 6
+
+// var greeting;
+// var hour = 13;
+
+// if(hour < 18){
+//     greeting = "Good day";
+// }
+// else{
+//     greeting = "Good evening";
+// }
+
+// Task 7
+
+// var timeInput = prompt("Enter the time in 24 hours format");
+
+// if(timeInput >= 0000 && timeInput < 1200){
+//     document.write("Good Morning");
+// }else if(timeInput >= 1200 && timeInput < 1700){
+//     document.write("Good Afternoon");
+// }else if(timeInput >= 1700 && timeInput < 2100){
+//     document.write("Good Evening");
+// }else if(timeInput >= 2100 && timeInput < 2359){
+//     document.write("Good Night");
+// }
+
+// Chap 14-16
