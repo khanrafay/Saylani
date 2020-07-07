febcd88f809e30285182d232096811e335e2be51
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2020-07-07 18:36:50 +0500

    Some more work on stop watch

--- StopWatch/app.js
+++ StopWatch/app.js
@@ -1,2 +1,8 @@
 var hours, minutes, seconds, miliseconds = 0;
 
+function timer(){
+    var milisecondsBox = document.getElementById("miliSecondsBox").innerHTML;
+    console.log(miliseconds)
+}
+
+timer();
