cdac4d3f8c08b214402806241524dedfd787b9bc
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2020-07-19 05:28:39 -0700

    Completed chap 43-48

--- Js Assigment/app.js
+++ Js Assigment/app.js
@@ -1910,37 +1910,83 @@
 
 // Task 1 
 
+// alert("Hello World")
+
+// Task 2
+
 // function phoneClicked(){
 //     alert('Thanks for purchasing from us');
 // }
 
-// Task 2
 
 
-// var view = document.getElementById("studentTable");
+
+// Task 3
+
+
 // var studentArray = [['0', 'Jhone', '10'],
 // ['1', 'Doe', '9'],
 // ['2', 'Marks', '10'],
 // ['3', 'James', '8']];
 
-
-// for (var i = 0; i < studentArray.length; i++) {
-//     document.getElementById("studentTable").innerHTML +=
-//         "<tr>" +
-//         "<td>" +
-//         " " + studentArray[i] + " " +
-//         "<button onclick=DeleteStudent(" + i + ")>Delete</button>" +
-//         "</td>" +
-//         "</tr>"
+// renderTable();
+// function renderTable() {
+//     if (studentArray.length > 1) {
+//         for (var i = 0; i < studentArray.length; i++) {
+//             document.getElementById("studentTable").innerHTML +=
+//                 "<table>" +
+//                 "<th>Index</th>" +
+//                 "<th>Name</th>" +
+//                 "<th>Class</th>" +
+//                 "<th>Action</th>" +
+//                 "<tr>" +
+//                 "<td>" +
+//                 " " + studentArray[i] + " " +
+//                 "<button onclick=DeleteStudent(" + i + ")>Delete</button>" +
+//                 "</td>" +
+//                 "</tr>" +
+//                 "</table>"
+//         }
+//     } else {
+//         document.getElementById("studentTable").innerHTML += "Empty Data"
+//     }
 // }
 
 // function DeleteStudent(indexValue) {
-//      studentArray = studentArray.slice(indexValue)
-//     console.log(studentArray);
-//     console.log(studentArray.length);
-
+//     document.getElementById("studentTable").innerHTML = "";
+//     studentArray.splice(indexValue, 1);
+//     renderTable();
 // }
 
-// Task 3
+// Task 4
+
+// var treeImage = document.getElementById("treeImage");
+
+// function mouseOver() {
+//     console.log(treeImage);
+//     treeImage.src = "Images/tree2.jpg"
+// }
+// function mouseOut() {
+//     console.log(treeImage)
+//     treeImage.src = "Images/tree1.jpg"
+// }
+
+
+// Task 5
+
+// var number = document.getElementById("count");
+// var count = 0;
+// console.log(count);
+// function increase() {
+//     count++;
+//     console.log(count);
+//     number.innerText = count;
+// }
+
+// function decrease() {
+//     count--;
+//     number.innerText = count;
+// }
+
 
 
